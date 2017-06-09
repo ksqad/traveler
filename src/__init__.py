a5a53d4c22ce9b7f09ceea1dc9524366c58da798
travelgeezer <travelgeezer@icloud.com>
2017-06-09 09:43:36 +0800

    [T] views: add root route

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,10 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
 
+import src.views
 from src.home.views import home_blueprint
+from src.todo_list.views import todo_list_blueprint
 
 # register our blueprints
 app.register_blueprint(home_blueprint)
+app.register_blueprint(todo_list_blueprint)
