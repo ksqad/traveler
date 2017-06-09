2d72707aca381e518a218a8ed8bb26483be9a7b2
travelgeezer <travelgeezer@icloud.com>
2017-06-09 09:42:35 +0800

    [Home] views: remove user and proper route

--- src/home/views.py
+++ src/home/views.py
@@ -5,16 +5,3 @@ home_blueprint = Blueprint(
     'home', __name__,
     template_folder='templates'
 )
-
-@home_blueprint.route('/')
-def hello():
-    return render_template('index.html')
-
-@home_blueprint.route('/user/<username>')
-def user(username):
-    return render_template('user.html', name=username)
-
-
-@home_blueprint.route('/proper/<age>')
-def peopro(age):
-    return render_template('proper.html',age=age)
